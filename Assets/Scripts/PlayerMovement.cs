94d1f9729c2dabea5aa255f98b8e6d6902f3d82d
Aravind_Barri <aravind.barri@xcubelabs.com>
2021-05-26 15:02:40 +0530

    player movement is done zigzag commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 Direction;
+
+    public float playerspeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Direction == Vector3.forward)
+            {
+                Direction = Vector3.left;
+            }
+            else
+            {
+                Direction = Vector3.forward ;
+            }
+        }
+        transform.Translate(Direction * playerspeed * Time.deltaTime);
     }
 }
