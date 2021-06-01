8552ff6c0a7b9c0532127838bf23d5258941d20a
Aravind_Barri <aravind.barri@xcubelabs.com>
2021-06-01 14:53:21 +0530

    Commit the changes till the falling tiles

--- Assets/Scripts/TileManagerScript.cs
+++ Assets/Scripts/TileManagerScript.cs
@@ -8,6 +8,9 @@ public class TileManagerScript : MonoBehaviour
     public GameObject currentTile;
     private static TileManagerScript instance;
 
+    Stack<GameObject> forwardTilePool;
+    Stack<GameObject> leftTilePool;
+
     public static TileManagerScript Instance
     {
         get
@@ -26,10 +29,15 @@ public class TileManagerScript : MonoBehaviour
     void Start()
     {
 
-        for (int i = 0; i < 10; i++)
+        /*for (int i = 0; i < 10; i++)
         {
             SpawnTile();
-        }
+        }*/
+
+        forwardTilePool = new Stack<GameObject>();
+        leftTilePool = new Stack<GameObject>();
+
+        
 
 
     }
@@ -39,10 +47,54 @@ public class TileManagerScript : MonoBehaviour
     {
 
     }
+
+    void CreateTiles(int value) //to create pool of tiles   
+    {
+        for(int i = 0; i<value; i++)
+        {
+            forwardTilePool.Push(Instantiate(tiles[0]));
+            leftTilePool.Push(Instantiate(tiles[1]));
+            forwardTilePool.Peek().SetActive(false);
+            leftTilePool.Peek().SetActive(false);
+            forwardTilePool.Peek().name = "ForwardTile";
+            leftTilePool.Peek().name = "LeftTile";
+        }
+    }
+    public void AddForwardTilePool(GameObject tempObj)
+    {
+        forwardTilePool.Push(tempObj);
+        forwardTilePool.Peek().SetActive(false);
+    }
+    public void AddLeftTilePool(GameObject tempObj)
+    {
+        leftTilePool.Push(tempObj);
+        leftTilePool.Peek().SetActive(false);
+    }
+
     public void SpawnTile()
     {
+        if(forwardTilePool.Count == 0 || leftTilePool.Count == 0)
+        {
+            CreateTiles(10);
+        }
+
         int index = Random.Range(0, tiles.Length);
-        currentTile = (GameObject)Instantiate(tiles[index], currentTile.transform.GetChild(index).position, Quaternion.identity);
+
+        if(index == 0)
+        {
+            GameObject temp = forwardTilePool.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currentTile.transform.GetChild(index).position;
+            currentTile = temp;
+        }
+        else if(index == 1)
+        {
+            GameObject temp = leftTilePool.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currentTile.transform.GetChild(index).position;
+            currentTile = temp;
+        }
+        //currentTile = (GameObject)Instantiate(tiles[index], currentTile.transform.GetChild(index).position, Quaternion.identity);
     }
 
 }
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour
 {
+    Rigidbody tempRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-
+        tempRigidbody = GetComponentInParent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,10 +18,31 @@ public class TileScript : MonoBehaviour
     }
     void OnTriggerExit(Collider other)
     {
-        TileManagerScript.Instance.SpawnTile();
+        
         if (other.tag == "Player")
         {
-            Debug.Log("Spawn TILE");
+            TileManagerScript.Instance.SpawnTile();
+            StartCoroutine("FallDown");
+            //Debug.Log("Spawn TILE");
+        }
+    }
+    IEnumerator FallDown()
+    {
+        yield return new WaitForSeconds(3);
+        tempRigidbody.isKinematic = false;
+        yield return new WaitForSeconds(1);
+        tempRigidbody.isKinematic = true;
+        if (tempRigidbody.gameObject.name == "ForwardTile")
+        {
+
+            TileManagerScript.Instance.AddForwardTilePool(tempRigidbody.gameObject);
+            //Debug.Log("Added to forward pool");
+        }
+        else if (tempRigidbody.gameObject.name == "LeftTile")
+        {
+
+            TileManagerScript.Instance.AddLeftTilePool(tempRigidbody.gameObject);
+            //Debug.Log("Added to left pool");
         }
     }
 }
